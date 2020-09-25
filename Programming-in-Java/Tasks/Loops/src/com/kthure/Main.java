f411fec31deb46f67c771f5a4c551d185efae9fb
Kent <kent.ho.thureson@gmail.com>
2020-09-25 12:36:18 +0200

    200925

---
+++ Programming-in-Java/Tasks/Loops/src/com/kthure/Main.java
@@ -0,0 +1,9 @@
+package com.kthure;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
---
+++ Programming-in-Java/Tasks/Loops/src/com/kthure/Recursion.java
@@ -0,0 +1,15 @@
+import javax.sound.midi.Soundbank;
+
+public class Recursion {
+   public static void main(String[] args) {
+      int result = sum(5, 10);
+      System.out.println(result);
+   }
+   public static int sum(int start, int end) {
+      if (end > start) {
+         return end + sum(start, end - 1);
+      } else {
+         return end;
+      }
+   }
+}
