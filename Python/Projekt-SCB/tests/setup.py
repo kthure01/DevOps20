56dff3648c0599f9a0f128e1a052e63fd38e0c68
kthure01 <kent.ho.thureson@gmail.com>
2020-12-28 11:23:48 +0100

    New project Calendar with names

--- Python/Projekt-SCB/tests/setup.py
+++ Python/Projekt-SCB/tests/setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='',
+    description='',
     version='',install_requires=[
         'pytest',
     ],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            ''
+            'do_something = folder.fileName:method_to_run
+            '
         ]
     }
 )
